ee2faa9cd65f775e17fc0646debf4b93fe105a3d
Michael Dever <mdever@live.ca>
2021-11-24 18:59:20 -0500

    Creating a function to count each instance of a letter in a given sentence and return an object containing the totals

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const results = {};
+
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const result1 = countLetters("lighthouse in the house");
+
+assertEqual(result1["l"], 1);
+assertEqual(result1["i"], 2);
+assertEqual(result1["g"], 1);
+assertEqual(result1["h"], 4);
+assertEqual(result1["t"], 2);
+assertEqual(result1["o"], 2);
+assertEqual(result1["u"], 2);
+assertEqual(result1["s"], 2);
+assertEqual(result1["e"], 3);
+assertEqual(result1["n"], 1);
