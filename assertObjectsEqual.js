c7cb67b195b612d281b3b25ce01ac4c5fd44deb2
Michael Dever <mdever@live.ca>
2021-11-25 14:37:45 -0500

    Function now displays objects correctly

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,4 +1,4 @@
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
   const keysArray1 = Object.keys(object1);
   const keysArray2 = Object.keys(object2);
 
@@ -17,14 +17,29 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+
   if (eqObjects(actual, expected) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
 assertObjectsEqual([1, 2, 3], [1, 2, 3]);
 assertObjectsEqual([1, 2, 3], ["1", 2, 3]);
 assertObjectsEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' });
