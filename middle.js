b2f07eda8c0d2fa2bcce4d21f2660158638b95ca
Michael Dever <mdever@live.ca>
2021-11-29 16:50:08 -0500

    Refactoring

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-};
-
 const middle = function(array) {
   let output = [];
 
@@ -35,19 +14,4 @@ const middle = function(array) {
   }
 };
 
-console.log(middle([1]));
-console.log(middle([1, 2]));
-
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
-
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
