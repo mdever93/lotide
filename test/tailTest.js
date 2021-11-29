01927956b7e9f697f91daecb3fe0ccf831a54f67
Michael Dever <mdever@live.ca>
2021-11-29 16:13:41 -0500

    Refactoring

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,5 @@
-const assertEqual = require('../assertEqual')
-const tail = require('../tail')
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
