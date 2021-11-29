21ac6a9efbbcb74f45123ad958aa7522fb945848
Michael Dever <mdever@live.ca>
2021-11-29 15:47:34 -0500

    Refactoring

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("String", "String");
-assertEqual("String1", "String2");
-assertEqual(5, 5);
-assertEqual(6, 10);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual')
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("String", "String");
+assertEqual("String1", "String2");
+assertEqual(5, 5);
+assertEqual(6, 10);
